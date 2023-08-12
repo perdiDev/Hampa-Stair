a590f5f7e679b5fe797ef6ea89e5ffacd033b5e1
PerdiDev <perdidev@gmail.com>
2023-08-12 20:29:31 +0800

    solve problem

--- main.ts
+++ main.ts
@@ -1,2 +1,16 @@
 // TODO: Change this function to make all the tests pass
-export function stair(num) {}
+export function stair(num: number) {
+    let ans: Array<Array<string>> = new Array();
+    for(let i=0; i<num; i++) {
+        let temp: Array<string> = new Array();
+        for(let j=0; j<i; j++) {
+            temp[j] = " ";
+        }
+
+        for(let j=i; j<num; j++) {
+            temp[j] = "*";
+        }
+        ans.push(temp);
+    }
+    return ans;
+}
